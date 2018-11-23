6b8203c12b770a2151dd6ee7287064563203d470
Xavier Yin <xavier.yin@kkday.com>
2018-11-23 12:08:03 +0800

    Add bold line

--- library/src/main/java/com/wdullaer/materialdatetimepicker/date/SimpleMonthView.java
+++ library/src/main/java/com/wdullaer/materialdatetimepicker/date/SimpleMonthView.java
@@ -46,11 +46,26 @@ public class SimpleMonthView extends MonthView {
         // gray out the day number if it's outside the range.
         if (mController.isOutOfRange(year, month, day)) {
             mMonthNumPaint.setColor(mDisabledDayTextColor);
+            mMonthNumSlashPaint.setColor(mDisabledDayTextColor);
             canvas.drawLine(
-                    x - MINI_DAY_NUMBER_TEXT_SIZE,
+                    x - MINI_DAY_NUMBER_TEXT_SIZE / 2,
+                    y - MINI_DAY_NUMBER_TEXT_SIZE / 3 - 1,
+                    x + MINI_DAY_NUMBER_TEXT_SIZE / 2,
+                    y - MINI_DAY_NUMBER_TEXT_SIZE / 3 - 1,
+                    mMonthNumSlashPaint
+            );
+            canvas.drawLine(
+                    x - MINI_DAY_NUMBER_TEXT_SIZE / 2,
                     y - MINI_DAY_NUMBER_TEXT_SIZE / 3,
-                    x + MINI_DAY_NUMBER_TEXT_SIZE,
+                    x + MINI_DAY_NUMBER_TEXT_SIZE / 2,
                     y - MINI_DAY_NUMBER_TEXT_SIZE / 3,
+                    mMonthNumSlashPaint
+            );
+            canvas.drawLine(
+                    x - MINI_DAY_NUMBER_TEXT_SIZE / 2,
+                    y - MINI_DAY_NUMBER_TEXT_SIZE / 3 + 1,
+                    x + MINI_DAY_NUMBER_TEXT_SIZE / 2,
+                    y - MINI_DAY_NUMBER_TEXT_SIZE / 3 + 1,
                     mMonthNumSlashPaint
             );
         } else if (mSelectedDay == day) {
